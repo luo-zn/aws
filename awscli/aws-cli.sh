c9aada8207eb3e6c8b2875bdeaefb6f6c9f12483
Zhennan <zhennan.luo@HQ.TA-MP.COM>
2020-04-01 14:11:03 +0800

    fix awscli/aws-cli.sh

--- awscli/aws-cli.sh
+++ awscli/aws-cli.sh
@@ -4,7 +4,31 @@
 
 function  awsCli()
 {
-  docker run -it  jenner/awscli  aws  $* 
+  docker run -it -v $AWS_CONFIG:/root/.aws jenner/awscli  aws  $* 
+}
+function initCfg(){  
+  export AWS_CONFIG=$(pwd)/aws-configure
+  if [ ! -d $AWS_CONFIG ];then
+    echo "Init AWS CONFIG!"
+    mkdir -p $AWS_CONFIG
+  fi
+  if [ ! -f $AWS_CONFIG/config ];then
+    cat > $AWS_CONFIG/config << EOF
+[default]
+region = eu-central-1
+EOF  
+  fi
+  if [ ! -f  $AWS_CONFIG/credentials ];then
+    cat > $AWS_CONFIG/credentials << EOF
+[default]
+aws_access_key_id = change_this_value
+aws_secret_access_key = change_this_value
+EOF
+fi
 }
 
+if [ ! -n "$AWS_CONFIG" ];then
+  echo "AWS_CONFIG does not exist!"
+  initCfg
+fi
 awsCli $*
