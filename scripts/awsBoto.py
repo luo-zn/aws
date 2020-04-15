0ffba2de3da8241bc7d73c4162a8b424c2e3ac65
lzn <jannanlo@163.com>
2020-04-15 15:55:35 +0800

    fix scripts/awsBoto.py

--- scripts/awsBoto.py
+++ scripts/awsBoto.py
@@ -11,7 +11,7 @@ def try_catch(f):
         try:
             return f(*args, **kwargs)
         except Exception as e:
-            print(getattr("__name__"), " Except: ", str(e))
+            print(f.__name__, " Except: ", str(e))
             return None
     return wrapFunc
 
@@ -55,6 +55,12 @@ class Lambda:
             res = self.client.create_alias(**kwargs)
         print("Alias", res)
 
+    @try_catch
+    def update_function_configuration(self,**kwargs):
+        print("*"*10, "Updating function configuration!", "*"*10)
+        kwargs.pop("Code")
+        print(self.client.update_function_configuration(**kwargs))
+
     def deploy(self, **kwargs):  
         print("*"*10, "Deploying Lambda!", "*"*10)      
         versionAlias = kwargs.pop("versionAlias",{})
@@ -63,6 +69,7 @@ class Lambda:
         if self.get_function(**kwargs):
             print("Updating  code!")
             resp = self.client.update_function_code(FunctionName=kwargs["FunctionName"], ZipFile=kwargs["Code"]["ZipFile"])
+            self.update_function_configuration(**kwargs)
         else:
             print("Creating lamda ", kwargs.get("FunctionName"))            
             resp = self.client.create_function(**kwargs)
@@ -72,25 +79,38 @@ class Lambda:
 
 class GitlabAws:
     def __init__(self):
-        pass 
+        self.env = os.environ
+    
+    def get_lambda_env(self):
+        # In gitlab web add env,  project -> settings -> CI/CD -> Variables
+        # variables format 1: LAMBDA_[ENV NAME] = VALUE        
+        # variables format 2: [REF NAME with capitalize]_LAMBDA_[ENV NAME] = VALUE        
+        lambda_env, env_prefix, ref_name = {}, "LAMBDA_", self.env.get("CI_COMMIT_REF_NAME","")
+        b_env_prefix = "%s_%s"%(ref_name.capitalize(),env_prefix)
+        for key in self.env.keys():          
+            if str(key).startswith(env_prefix):
+                lambda_env[key.split(env_prefix)[-1]] = self.env[key]
+            if str(key).startswith(b_env_prefix):
+                lambda_env[key.split(b_env_prefix)[-1]] = self.env[key]
+        return lambda_env
 
     def read_env(self):
-        env = os.environ
-        branch = env["CI_COMMIT_REF_NAME"]
+        branch = self.env["CI_COMMIT_REF_NAME"]
         name = branch.capitalize()
         if branch == "master":
             name = "Prod"
-        return {"FunctionName":env["CI_PROJECT_NAME"], "Runtime": "go1.x", "Role":env["AWS_ROLE"], 
-        "Handler":env["GO_BIN"],"Code": {"ZipFile":env["CODE_ZIP"]},
+        return {"FunctionName":self.env["CI_PROJECT_NAME"], "Runtime": "go1.x", "Role":self.env["AWS_ROLE"], 
+        "Handler":self.env["GO_BIN"],"Code": {"ZipFile":self.env["CODE_ZIP"]},"Environment":{'Variables':  self.get_lambda_env()},
         "versionAlias":{"Description":"%s branch code!" % branch, "Name":name}
         }
 
     def main(self):
-        # lda = {"FunctionName":"myFunction", "Runtime": "go1.x", "Role":"arn:aws:iam::xxxxxxxxx:role/gitlab-ci", "Handler":"myFunction-main",
-        # "Code": {"ZipFile":"/data/lzn-test-myFunction-main.zip"},
+        # lda = {"FunctionName":"myFunction", "Runtime": "go1.x", "Role":"arn:aws:iam::xxxxxxx:role/gitlab-ci", "Handler":"myFunction-main",
+        # "Code": {"ZipFile":"/data/lzn-test-myFunction-main.zip"}, "Environment":{'Variables':  self.get_lambda_env()},
         # "versionAlias":{"Description":"Test", "Name":"Test"}
         # }
         Lambda().deploy(**self.read_env())
+        # Lambda().deploy(**lda)
 
 if __name__ == "__main__":
   GitlabAws().main()
